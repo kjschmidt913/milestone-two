da26a57b4ef7b0485162be0bf42b19a7cee1f124
KJ <kj.schmidt913@gmail.com>
2019-02-19 10:07:42 -0500

    able to print deck and choose card

--- milestone-two.py
+++ milestone-two.py
@@ -1,18 +1,25 @@
 import random
 
-suits = ('Hearts', 'Diamonds', 'Spades', 'Clubs')
-ranks = ('Two', 'Three', 'Four', 'Five', 'Six', 'Seven', 'Eight', 'Nine', 'Ten', 'Jack', 'Queen', 'King', 'Ace')
-values = {'Two':2, 'Three':3, 'Four':4, 'Five':5, 'Six':6, 'Seven':7, 'Eight':8, 'Nine':9, 'Ten':10, 'Jack':10, 'Queen':10, 'King':10, 'Ace':11}
-
-
-class Deck:  
+class Deck():
     def __init__(self):
-        self.deck = []
-        for suit in suits:
+        suites = ('Spades', 'Hearts', 'Diamonds', 'Clubs')
+        ranks = ('2', '3', '4', '5', '6', '7', '8', '9', '10', "Jack", "Queen", "King", "Ace")
+        self.fullDeck = []
+        for suite in suites:
             for rank in ranks:
-                pair = (suit, rank)
-                self.deck.append(pair)
+                self.fullDeck.append("%s of %s" % (rank, suite))
 
-    def shuffle(self):
-        random.shuffle(self.deck)
+    def pick_item(self):
+        card_key = random.randint(0, len(self.fullDeck)-1)  
+        new_card = self.fullDeck[card_key] 
+        del self.fullDeck[card_key]  
+        return new_card
+
+    def missing_card(self):
+        print (self.fullDeck)
+
+d1 = Deck()
+card1 = d1.pick_item()
+
+print(d1.fullDeck)
 
